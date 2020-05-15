c6e6c6dce460b56a24245e83e50e7ebb15c94fcb
Jack Hyland <101603196@student.swin.edu.au>
2020-05-15 19:49:27 +1000

    name on homepage feature

--- Client/login.php
+++ Client/login.php
@@ -1,10 +1,15 @@
 <?php
-    $host = "localhost";
-	$user = "root";
-    $password = "dp1-2020";
-    $sql_db = "voces_db";
-    $link = mysqli_connect($host, $user, $password, $sql_db);
+    require_once("settings.php");
+		
+    $link = @mysqli_connect(
+        $host,
+        $user,
+        $password,
+        $sql_db
+    );
 
+    //$email = htmlspecialchars($_GET["email"]);
+    //$pwd = htmlspecialchars($_GET["pwd"]);
     $email = $_POST["email"];
     $pwd = $_POST["pwd"];
 
@@ -15,13 +20,13 @@
     }
     else
     {
+        $loginCheckQuery = "SELECT Id, Email, Username, Password FROM Users WHERE Email='$email' AND Password='$pwd'";
 
-
-        $loginCheckQuery = "SELECT Id,Email,Password FROM USERS WHERE Email = '$email' AND password = '$pwd'";
-
+        
         if ($existCheck = mysqli_query($link, $loginCheckQuery))
         {
             echo "Logged in successfully.";
+            $username1 = mysqli_fetch_assoc($existCheck);
             $loggedInUser = mysqli_fetch_object($existCheck);
             $loggedInUserId = $loggedInUser->Id;
             //send the credentials of the logged in user to the front-end
@@ -29,14 +34,17 @@
             $loggedInUserJSON = json_encode($loggedInUserObject);
             echo $loggedInUserJSON;
             session_start();
-            $_SESSION["Id"] = $loggedInUserId; 
-            header("Location: http://localhost/idea-sharing-platform/Client/home.php");
+            $_SESSION["Id"] = $loggedInUserId;
+            $_SESSION["username"] = $username1['Username'];  
+            header("Location: https://mercury.swin.edu.au/cos10011/s101603196/softwareDev/idea-sharing-platform/Client/home.php");
             exit();
+ 
+            
         }
         else
         {
             echo "Incorrect username or password.";
-            header("Location: http://localhost/idea-sharing-platform/Client/login.html");
+            header("Location: https://mercury.swin.edu.au/cos10011/s101603196/softwareDev/idea-sharing-platform/Client/login.html");
             exit();
         }
     
---
+++ Client/settings.php
@@ -0,0 +1,9 @@
+<?php
+	$host = "feenix-mariadb.swin.edu.au";
+	$user = "s101603196"; // your user name
+	$password = "asd345dfg"; // your password (date of birth ddmmyy unless changed)
+	$sql_db = "s101603196_db"; // your database
+?>
+	
+	
+	
