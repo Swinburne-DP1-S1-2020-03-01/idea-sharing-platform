86ebdce0555849f38a08707e94eea4ea1c58eb26
Lachlan McCaskill <101627963@student.swin.edu.au>
2020-05-19 18:55:00 +1000

    Reverted change that would remove some formatting from the database.

--- Client/save-articles.php
+++ Client/save-articles.php
@@ -5,8 +5,6 @@
     require_once("settings.php");
 
     $content = $_POST["content"];
-    $content = rtrim($content, "</p>");
-    $content = ltrim($content, "<p>");
     $title = $_POST["title"];
     $date = date("Y-m-d"); 
     
