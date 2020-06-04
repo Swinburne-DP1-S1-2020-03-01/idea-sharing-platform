d074760f24a9aa53e2dde43874fd4983aeaa9eac
JackHyland <101603196@student.swin.edu.au>
2020-06-04 12:58:36 +1000

    Jacks Final CI

---
+++ valimate/test.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+    <html>
+        <head>
+            <title>My Website</title>
+        </head> 
+        <body>
+            <section role="main">
+                <h1>Valimate Test Server</h1>
+                <p>This is some invalid, dummy HTML</p>
+            </section>
+        </body>
+    </html>
